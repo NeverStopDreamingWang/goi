4f466d5335eb489d7c76b012531ab8ab63e8b2df
王佳函 <mr_jia_han@qq.com>
2024-01-05 02:51:43 +0800

    hgee 测试用例调整

--- example/middleware/middleware.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "github.com/NeverStopDreamingWang/hgee"
-
-// 请求中间件
-func RequestMiddleWare(request *hgee.Request) any {
-	// fmt.Println("请求中间件", request.Object.URL)
-	return nil
-}
