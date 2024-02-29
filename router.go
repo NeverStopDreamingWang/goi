09887069f926928062701770961e0fc27ef05afe
王佳函 <mr_jia_han@qq.com>
2024-03-01 04:50:00 +0800

    修复-路由匹配错误问题

--- router.go
+++ router.go
@@ -58,7 +58,7 @@ func (router *metaRouter) isUrl(UrlPath string) {
 		var reString string
 		if len(params) == 0 { // 无参数直接匹配
 			if len(Irouter.includeRouter) == 0 || Irouter.viewSet.File != "" {
-				reString = includePatternUri + "&"
+				reString = includePatternUri + "$"
 			} else if strings.HasSuffix(includePatternUri, "/") == false {
 				reString = includePatternUri + "/"
 			} else {
@@ -169,7 +169,7 @@ func routeResolution(requestPattern string, includeRouter map[string]*metaRouter
 		var reString string
 		if len(params) == 0 { // 无参数直接匹配
 			if len(Irouter.includeRouter) == 0 || Irouter.viewSet.File != "" {
-				reString = includePatternUri + "&"
+				reString = includePatternUri + "$"
 			} else if strings.HasSuffix(includePatternUri, "/") == false {
 				reString = includePatternUri + "/"
 			} else {
