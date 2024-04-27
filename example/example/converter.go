f23081e37e5a23105883121a3fed5234307fe22b
王佳函 <mr_jia_han@qq.com>
2024-04-27 19:54:07 +0800

    调整-示例-路由转换器注册函数

--- example/example/converter.go
+++ example/example/converter.go
@@ -2,8 +2,8 @@ package example
 
 import "github.com/NeverStopDreamingWang/goi"
 
-func RegisterMyConverter() {
-	// 注册一个路由转换器
+func init() {
+	// 注册路由转换器
 
 	// 手机号
 	goi.RegisterConverter("phone", `(1[3456789]\d{9})`)
