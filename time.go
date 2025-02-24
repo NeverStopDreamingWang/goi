4ecc302e6259873e5e72fec63b63ed457a57316e
函 <mr_jia_han@qq.com>
2025-02-24 15:40:15 +0800

    添加-全局 goi.GetLocation 获取时区方法

--- time.go
+++ time.go
@@ -4,6 +4,14 @@ import (
 	"time"
 )
 
+// 获取当前时区
+func GetLocation() *time.Location {
+	if Settings != nil {
+		return Settings.GetLocation()
+	}
+	return time.Local
+}
+
 // 获取当前时区时间
 func GetTime() time.Time {
 	return time.Now().In(Settings.GetLocation())
