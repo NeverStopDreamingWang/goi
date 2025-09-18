26b7ec71c944320c1c15b273af6dfcaca4e8dd4a
函 <mr_jia_han@qq.com>
2025-09-18 09:07:54 +0800

    提交-i18n文件

---
+++ internal/i18n/i18n.go
@@ -0,0 +1,37 @@
+package i18n
+
+import (
+	"embed"
+	"encoding/json"
+
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+//go:embed all:lang/*
+var langFS embed.FS
+var bundle *i18n.Bundle
+var I18n *i18n.Localizer
+
+func init() {
+	bundle = i18n.NewBundle(language.Chinese)
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	_, _ = bundle.LoadMessageFileFS(langFS, "lang/zh_cn.json")
+	_, _ = bundle.LoadMessageFileFS(langFS, "lang/en_us.json")
+	I18n = i18n.NewLocalizer(bundle, "zh_cn")
+}
+
+// 设置语言
+func SetLocalize(langs ...string) {
+	I18n = i18n.NewLocalizer(bundle, langs...)
+}
+
+func T(messageID string, data ...map[string]interface{}) string {
+	localizeConfig := &i18n.LocalizeConfig{
+		MessageID: messageID,
+	}
+	if len(data) > 0 && data[0] != nil {
+		localizeConfig.TemplateData = data[0]
+	}
+	return I18n.MustLocalize(localizeConfig)
+}
