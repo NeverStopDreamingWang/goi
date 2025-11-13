a72c2356f8a0ae9bc6b632c8989dd1f73f902636
函 <mr_jia_han@qq.com>
2025-11-13 10:13:04 +0800

    添加-ViewSet 添加对 HEAD、OPTIONS 方法自动处理

--- view.go
+++ view.go
@@ -2,6 +2,7 @@ package goi
 
 import (
 	"errors"
+	"net/http"
 
 	"github.com/NeverStopDreamingWang/goi/internal/i18n"
 )
@@ -13,39 +14,55 @@ type HandlerFunc func(request *Request) interface{}
 
 // 路由视图
 type ViewSet struct {
-	GET      HandlerFunc
-	HEAD     HandlerFunc
-	POST     HandlerFunc
-	PUT      HandlerFunc
-	PATCH    HandlerFunc
-	DELETE   HandlerFunc
-	CONNECT  HandlerFunc
-	OPTIONS  HandlerFunc
-	TRACE    HandlerFunc
+	GET      HandlerFunc // 获取资源
+	HEAD     HandlerFunc // 获取响应头
+	POST     HandlerFunc // 创建资源
+	PUT      HandlerFunc // 更新资源
+	PATCH    HandlerFunc // 部分更新
+	DELETE   HandlerFunc // 删除资源
+	CONNECT  HandlerFunc // 建立隧道
+	OPTIONS  HandlerFunc // 查询支持的方法
+	TRACE    HandlerFunc // 回显请求
 	NoMethod HandlerFunc // 未注册或不支持的HTTP方法时的处理函数，默认返回 405
 }
 
+// GetHandlerFunc 获取视图方法
+//
+// 参数:
+//   - method string: HTTP方法
+//
+// 返回:
+//   - HandlerFunc: 处理函数
+//   - error: 错误信息
 func (viewSet ViewSet) GetHandlerFunc(method string) (HandlerFunc, error) {
 	var handlerFunc HandlerFunc
 	var err error
 	switch method {
-	case "GET":
+	case http.MethodGet:
 		handlerFunc = viewSet.GET
-	case "HEAD":
+	case http.MethodHead:
 		handlerFunc = viewSet.HEAD
-	case "POST":
+		// HEAD 未定义时，回退到 GET，但不返回 body
+		if handlerFunc == nil && viewSet.GET != nil {
+			handlerFunc = viewSet.GET
+		}
+	case http.MethodPost:
 		handlerFunc = viewSet.POST
-	case "PUT":
+	case http.MethodPut:
 		handlerFunc = viewSet.PUT
-	case "PATCH":
+	case http.MethodPatch:
 		handlerFunc = viewSet.PATCH
-	case "DELETE":
+	case http.MethodDelete:
 		handlerFunc = viewSet.DELETE
-	case "CONNECT":
+	case http.MethodConnect:
 		handlerFunc = viewSet.CONNECT
-	case "OPTIONS":
+	case http.MethodOptions:
 		handlerFunc = viewSet.OPTIONS
-	case "TRACE":
+		// OPTIONS 未定义时，使用默认处理函数
+		if handlerFunc == nil {
+			handlerFunc = defaultOptionsHandler
+		}
+	case http.MethodTrace:
 		handlerFunc = viewSet.TRACE
 	}
 	if handlerFunc == nil {
@@ -59,4 +76,60 @@ func (viewSet ViewSet) GetHandlerFunc(method string) (HandlerFunc, error) {
 		}
 	}
 	return handlerFunc, err
+}
+
+// GetMethods 获取允许的HTTP方法列表
+//
+// 返回:
+//   - []string: 允许的HTTP方法列表
+func (viewSet ViewSet) GetMethods() []string {
+	var methods []string
+	if viewSet.GET != nil {
+		methods = append(methods, http.MethodGet)
+	}
+	if viewSet.HEAD != nil {
+		methods = append(methods, http.MethodHead)
+	} else if viewSet.GET != nil {
+		methods = append(methods, http.MethodHead)
+	}
+	if viewSet.POST != nil {
+		methods = append(methods, http.MethodPost)
+	}
+	if viewSet.PUT != nil {
+		methods = append(methods, http.MethodPut)
+	}
+	if viewSet.PATCH != nil {
+		methods = append(methods, http.MethodPatch)
+	}
+	if viewSet.DELETE != nil {
+		methods = append(methods, http.MethodDelete)
+	}
+	if viewSet.CONNECT != nil {
+		methods = append(methods, http.MethodConnect)
+	}
+	methods = append(methods, http.MethodOptions)
+	if viewSet.TRACE != nil {
+		methods = append(methods, http.MethodTrace)
+	}
+	return methods
+}
+
+// defaultOptionsHandler 默认的 OPTIONS 请求处理函数
+//
+// 当用户未自定义 OPTIONS 处理函数时，自动调用此方法
+// 返回 200 状态码，并设置 Content-Length 响应头
+// Allow 响应头由框架自动设置
+//
+// 参数:
+//   - request *Request: HTTP请求对象
+//
+// 返回:
+//   - Response: HTTP响应对象，包含允许的方法列表
+func defaultOptionsHandler(request *Request) interface{} {
+	response := Response{
+		Status: 200, // http.StatusOK
+		Data:   nil,
+	}
+	response.Header().Set("Content-Length", "0")
+	return response
 }
