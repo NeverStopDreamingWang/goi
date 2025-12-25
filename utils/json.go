0779094dcc1f283a1a66e2f1a6547c21a9cd6828
函 <mr_jia_han@qq.com>
2025-12-25 09:32:39 +0800

    添加-utils/json.go SaveJSON 帮助方法

--- utils/json.go
+++ utils/json.go
@@ -19,6 +19,7 @@ func LoadJSON(filePath string, value any) error {
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 
 	// 创建 JSON 解码器
 	decoder := json.NewDecoder(file)
@@ -27,7 +28,29 @@ func LoadJSON(filePath string, value any) error {
 	if err != nil {
 		return err
 	}
-	err = file.Close()
+	return nil
+}
+
+// SaveJSON 通用的将指定的结构体保存到 JSON 文件
+//
+// 参数:
+//   - filePath string: JSON 文件路径
+//   - value any: 目标结构体
+//
+// 返回:
+//   - error: 错误信息
+func SaveJSON(filePath string, value any) error {
+	// 打开 JSON 文件
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 使用 json 编码器将数据写入文件
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(value)
 	if err != nil {
 		return err
 	}
